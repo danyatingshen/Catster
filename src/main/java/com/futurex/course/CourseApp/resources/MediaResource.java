62ebf1b7ac6dfd9e127753128a862a83ad1d8286
amshen <amshen@hubspot.com>
2022-01-16 19:31:50 -0600

    applicationerror fix

--- src/main/java/com/futurex/course/CourseApp/resources/MediaResource.java
+++ src/main/java/com/futurex/course/CourseApp/resources/MediaResource.java
@@ -5,12 +5,10 @@ import java.util.concurrent.ExecutionException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.futurex.course.CourseApp.model.processing.MediaProcessingRequest;
 import com.futurex.course.CourseApp.model.processing.MediaProcessingResult;
-import com.futurex.course.CourseApp.model.user.User;
 
 @RestController
 public class MediaResource {
