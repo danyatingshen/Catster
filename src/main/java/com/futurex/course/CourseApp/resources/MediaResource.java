edf0641f6e50371e9d916482e128d00f2ba0033c
amshen <amshen@hubspot.com>
2022-02-10 23:02:10 -0600

    updated channel

--- src/main/java/com/futurex/course/CourseApp/resources/MediaResource.java
+++ src/main/java/com/futurex/course/CourseApp/resources/MediaResource.java
@@ -2,17 +2,22 @@ package com.futurex.course.CourseApp.resources;
 
 import java.util.concurrent.ExecutionException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.futurex.course.CourseApp.model.media.MediaProcessingRequest;
+import com.futurex.course.CourseApp.manager.media.MediaManager;
 import com.futurex.course.CourseApp.model.media.MediaProcessingResult;
-
 @RestController
 public class MediaResource {
+
+  @Autowired
+  private MediaManager mediaManager;
+
   @GetMapping("/media/debug")
   @CrossOrigin
   public String test() throws InterruptedException, ExecutionException {
@@ -21,7 +26,10 @@ public class MediaResource {
 
   @PostMapping("/media/process")
   @CrossOrigin
-  public MediaProcessingResult processingMedia(@RequestBody MediaProcessingRequest mediaProcessingRequest) throws InterruptedException, ExecutionException {
-    return new MediaProcessingResult("userId","logId","taskId","hello");
+  public MediaProcessingResult mediaProcessor(@RequestParam("userId") String userId,
+                                              @RequestParam("logId") String logId,
+                                              @RequestParam("file") MultipartFile file) throws InterruptedException, ExecutionException {
+   return mediaManager.processMedia(userId,logId,file);
+
   }
 }
